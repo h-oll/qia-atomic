0734ebdc9f155415d9092ab53f071b5d286fc83c
lecascyril <45956179+lecascyril@users.noreply.github.com>
2020-03-04 11:24:57 +0100

    Add files via upload

--- qpzlib/qpzlib.py
+++ qpzlib/qpzlib.py
@@ -9,6 +9,7 @@ class qpzlib:
 
         self.K = backend_mapping["K"]
         self.T = backend_mapping["T"]
+        self.invT = backend_mapping["invT"]
 
         self.PREP = lambda *args: backend_mapping["PREP"](*args, node=node) 
         self.MEAS = backend_mapping["MEAS"]
@@ -63,4 +64,86 @@ class qpzlib:
             if k == 3: self.Y(q)
             yield q
 
+
     
+    def CSWAP(self, q0, q1, q2):
+
+        """
+        controll swap on two qubits q1 and q2, q0 is a controll qubit
+        implementation from https://www.mathstat.dal.ca/~selinger/quipper/doc/Quipper-Libraries-GateDecompositions.html
+        params : 
+        q0,q1,q2: qubits
+
+        """
+        self.CNOT(q2,q1)
+        self.H(q2)
+        self.T(q0)
+        self.T(q1)
+        self.T(q2)
+        self.CNOT(q1,q0)
+        self.CNOT(q2,q1)
+        self.CNOT(q0,q2)
+        self.invT(q1)
+        self.T(q2)
+        self.CNOT(q0,q1)
+        self.invT(q0)
+        self.invT(q1)
+        self.CNOT(q2,q1)
+        self.CNOT(q0,q2)
+        self.CNOT(q1,q0)
+        self.H(q2)
+        self.CNOT(q2,q1)
+        return
+
+    def swap_test(self, conn, q1, q2):
+
+        """
+        launch the swap test
+        params:
+            conn: string, node to do the swap
+            q1, q2: iterable of qubits
+
+        """
+
+        q=zip(q1,q2)
+        q0 = self.PREP()
+        self.H(q0)
+
+        for q1,q2 in q:
+            self.CSWAP(q0, q1, q2)
+            print(self.MEAS(q1), self.MEAS(q2))
+
+        self.H(q0)
+        m = self.MEAS(q0)
+
+        print ('q0 measure is ', m)
+        
+        return m
+
+    def qrng(self) :
+
+        """
+        return random 0 or 1 via hadarmard gate
+        param:
+            location_strings: string, node where the q.h is happening, 'Alice' by default
+        
+        """
+        q=self.PREP()
+        self.H(q)
+        number = self.MEAS()
+        print('Outcome of the measure:', number)
+        return number
+
+    def pauli_prep(self, bit, base):
+        if base == 1: 
+            if bit == 0 : q = self.H(self.PREP())
+            elif bit == 1 : q = self.H(self.X(self.PREP()))
+        elif base == 2: 
+            if bit == 0 : q = self.PREP()
+            elif bit == 1 : q = self.X(self.PREP())
+        if base == 3: 
+            if bit == 0 : q = self.K(self.PREP())
+            elif bit == 1 : q = self.K(self.X(self.PREP()))
+        else: raise NameError("Cannot prepare this state")
+        return q
+

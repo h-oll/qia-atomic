9463f7e75b03d826970fecec489ae7560017bba4
Harold Ollivier <harold.ollivier@dealsights.com>
2020-04-19 18:33:07 +0200

    dispatched examples in various files

---
+++ qpzlib/examples/netsquid.py
@@ -0,0 +1,14 @@
+import os,sys,inspect
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir) 
+
+from qpzlib import qpzlib 
+
+###################################
+## Examples using NetSquid
+## 
+## 
+##
+
+
--- qpzlib/examples/quunetsim.py
+++ qpzlib/examples/quunetsim.py
@@ -6,41 +6,6 @@ sys.path.insert(0, parent_dir)
 from qpzlib import qpzlib 
 
 ###################################
-## Examples using SimulaQron / CQC
-
-def simulaqron():
-
-    from cqc.pythonLib import CQCConnection
-    from mappings.simulaqron import mapping
-
-    with CQCConnection("Alice") as Alice:
-
-        _ = qpzlib(mapping, Alice)
-
-        #use library to prepare a single qubit
-        # all commands are applied locally: no need to specify the node
-        q = _.PREP()
-
-        # show info about created qubit
-        _.DISP(q)
-
-        # flip the qubit
-        _.X(q)
-        
-        # sending to Bob (need to pass the arguments using the syntax used by the backend used)
-        _.SEND(q, "Bob") 
-
-
-    with CQCConnection("Bob") as Bob:
-        _ = qpzlib(mapping, Bob)
-
-        # receive a qubit
-        q = _.RECV()
-
-        # show info about this qubit
-        _.DISP(q)
-
-###################################
 ## Examples using QuNetSim
 ## To use QuNetSim, add its path to
 ## PYTHONPATH
@@ -102,6 +67,5 @@ def qunetsim():
     host_bob.run_protocol(target_protocol, (host_alice.host_id,))
 
 if __name__ == '__main__':
-    simulaqron()
-    #qunetsim()
+    qunetsim()
 

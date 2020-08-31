d03212e95316e274b30926a28b93437728f07b4b
Harold Ollivier <harold.ollivier@dealsights.com>
2020-08-31 15:31:29 +0200

    directory organization to follow package templates

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='qpz_atomics',
+      version='0.1',
+      description='Quantum Protocol Zoo Atomic Functions Library',
+      url='http://github.com/h-oll/qia-atomic-qpz_atomics',
+      author='Harold Ollivier',
+      author_email='harold.ollivier@mines.org',
+      license='MIT',
+      packages=['qpz_atomics'],
+      zip_safe=False)
